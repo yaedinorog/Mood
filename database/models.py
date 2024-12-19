d98297898a745f789edf56e9f17df24f65b4bc6c
Iamuniicorn <rud.yar@list.ru>
2024-12-20 00:31:11 +0300

    Started bd backend logic

--- database/models.py
+++ database/models.py
@@ -10,35 +10,35 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'User'
 
-    id = Column(Integer, primary_key=True)
-    tg_id = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tg_id = Column(String, unique=True)
     accept_pc = Column(Boolean)
     phone = Column(String)
 
 class user_pair(Base):
     __tablename__ = 'user_pair'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_id1 = Column(Integer, ForeignKey('User.id'))
     user_id2 = Column(Integer, ForeignKey('User.id'))
 
 class mood(Base):
     __tablename__ = 'mood'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
 
 class chat(Base):
     __tablename__ = 'chat'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_pair_id = Column(Integer, ForeignKey('user_pair.id'))
     mood_id = Column(Integer, ForeignKey('mood.id'))
 
 class history_mood(Base):
     __tablename__ = 'history_name'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     date = Column(Date)
     chat_id = Column(Integer, ForeignKey('chat.id'))
     mood_id = Column(Integer, ForeignKey('mood.id'))
@@ -46,20 +46,20 @@ class history_mood(Base):
 class attach_type(Base):
     __tablename__ = 'attach_type'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
 
 class attach(Base):
     __tablename__ = 'attach'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     attach_type_id = Column(Integer, ForeignKey('attach_type.id'))
     file_path = Column(String)
 
 class Message(Base):
     __tablename__ = 'message'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     chat_id = Column(Integer, ForeignKey('chat.id'))
     sender_id = Column(Integer, ForeignKey('User.id'))
     text = Column(String)
--- database/test_data.py
+++ database/test_data.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import sessionmaker
-from models import User, engine
+from models import User, engine, user_pair
+from sqlalchemy import and_
 
 def get_session():
     Session = sessionmaker(bind=engine)
@@ -7,7 +8,8 @@ def get_session():
 
 def add_test_data():
     session = get_session()
-    session.add(User(id = 1, tg_id = "@Iamuniicorn", accept_pc=True, phone = '+79175680112'))
+    session.add(User(tg_id = "@Iamuniicorn", accept_pc=True, phone = '+79171310112'))
+    session.add(User(tg_id="@Bebricks", accept_pc=True, phone='+79173540112'))
     session.commit()
 
 def get_data_user():
@@ -15,3 +17,50 @@ def get_data_user():
     users = session.query(User).all()
     for user in users:
         print(user.__dict__)
+
+def checkIfUserExists(tg_id):
+    session = get_session()
+    existing_user = session.query(User.tg_id).filter(User.tg_id.in_([tg_id])).all()
+    return [user for user in existing_user]
+
+def Login(tg_id):
+    session = get_session()
+    if tg_id[0] != '@':
+        raise TypeError
+    is_in_db = checkIfUserExists(tg_id)
+    if is_in_db:
+        print("User correct")
+    else:
+        raise TypeError
+
+def Registration(tg_id, phone):
+    session = get_session()
+    if tg_id[0] != '@':
+        raise TypeError
+    is_in_db = checkIfUserExists(tg_id)
+    if is_in_db:
+        raise TypeError
+    else:
+        session.add(User(tg_id=tg_id, accept_pc=True, phone=phone))
+        session.commit()
+        print("Successfully added")
+
+def AddNewChat(user_id1, user_id2):
+    user_id1, user_id2 = min(user_id1, user_id2), max(user_id1, user_id2)
+    session = get_session()
+
+    exists = session.query(user_pair).filter(
+        and_(
+            user_pair.user1_id == user_id1,
+            user_pair.user2_id == user_id2
+        )
+    ).first()
+
+    if not exists:
+        # If not exists, insert the pair
+        new_pair = user_pair(user_id1=user_id1, user_id2=user_id2)
+        session.add(new_pair)
+        session.commit()
+        print("Pair added to the database.")
+    else:
+        print("Pair already exists in the database.")
